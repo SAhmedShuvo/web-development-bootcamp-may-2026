d11864cb3bef279b9168a35381bca580343711e7
sabbir <sabbir.cou15@gmail.com>
2026-05-15 09:35:55 +0600

    complete personal expense tracker project

---
+++ SAhmedShuvo/backend/middlewares/app.middleware.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+module.exports = (app) => {
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(helmet());
+  app.use(cors({
+    origin: "http://localhost:3000", // Allow requests from your frontend
+    credentials: true,
+  }));
+};
